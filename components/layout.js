1a70bd71ad155542c715bc91958e0da993ad9a5e
paul.campbell5 <paul.campbell@moneysupermarket.com>
2023-03-14 14:11:03 +0000

    a form

---
+++ components/layout.js
@@ -0,0 +1,8 @@
+export default function Layout({ children }) {
+  return (
+    <>
+      <h1>Layout here</h1>
+      <main>{children}</main>
+    </>
+  );
+}
---
+++ pages/post.js
@@ -0,0 +1,31 @@
+import { promisify } from 'util';
+import bodyParser from 'body-parser';
+
+import Layout from '../components/layout';
+
+const getBody = promisify(bodyParser.urlencoded());
+
+export async function getServerSideProps({ req, res }) {
+  if (req.method === 'POST') {
+    await getBody(req, res);
+  }
+  return {
+    props: {
+      title: req.body?.title || '',
+    },
+  };
+}
+
+export default function Post(props) {
+  return (
+    <Layout>
+      <main>
+        <form action="/post" method="post">
+          <input type="text" name="title" defaultValue={props.title} />
+          <button type="submit">Submit</button>
+          <span>{props.title}</span>
+        </form>
+      </main>
+    </Layout>
+  );
+}
